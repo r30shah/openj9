1a1ffc70e81cbb20745806cff9c8ef26835f8751
Rahil Shah <rahil@ca.ibm.com>
2018-09-11 18:27:47 -0400

    Add Primitive CondyTests for JDK11
    Add Java 11 tests for Constant Dynamic mainly involves
    String object, NULL object and Primitive data types
    objects. Variation covers running this test in interpreted mode
    as well as specific JIT compilation modes which checks out
    JIT's handling of resolved and unresolved Constant Dynamic cases.
    
    Signed-off-by: Rahil Shah <rahil@ca.ibm.com>

---
+++ test/functional/Java11andUp/src/org/openj9/test/condy/PrimitiveCondyTest.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright (c) 2018, 2018 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/
+ * or the Apache License, Version 2.0 which accompanies this distribution and
+ * is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following
+ * Secondary Licenses when the conditions for such availability set
+ * forth in the Eclipse Public License, v. 2.0 are satisfied: GNU
+ * General Public License, version 2 with the GNU Classpath
+ * Exception [1] and GNU General Public License, version 2 with the
+ * OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
+package org.openj9.test.condy;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
+
+public class PrimitiveCondyTest {
+	 
+	@Test(groups = { "level.sanity" })
+	public void testCondyNull() {
+		Assert.assertNull(PrimitiveCondyMethods.condy_return_null());
+		Assert.assertNull(PrimitiveCondyMethods.condy_return_null());
+		Assert.assertNull(PrimitiveCondyMethods.condy_return_null());
+	}
+
+	@Test(groups = { "level.sanity" })
+	public void testCondyInt() {
+		Assert.assertEquals(PrimitiveCondyMethods.condy_return_int(), 123432);
+		Assert.assertEquals(PrimitiveCondyMethods.condy_return_int(), 123432);
+		Assert.assertEquals(PrimitiveCondyMethods.condy_return_int(), 123432);
+	}
+
+	@Test(groups = { "level.sanity" })
+	public void testCondyFloat() {
+		Assert.assertEquals(PrimitiveCondyMethods.condy_return_float(), 10.12F);
+		Assert.assertEquals(PrimitiveCondyMethods.condy_return_float(), 10.12F);
+		Assert.assertEquals(PrimitiveCondyMethods.condy_return_float(), 10.12F);
+	}
+
+	@Test(groups = { "level.sanity" })
+	public void testCondyLong() {
+		Assert.assertEquals(PrimitiveCondyMethods.condy_return_long(), 100000000000L);
+		Assert.assertEquals(PrimitiveCondyMethods.condy_return_long(), 100000000000L);
+		Assert.assertEquals(PrimitiveCondyMethods.condy_return_long(), 100000000000L);
+	}
+
+	@Test(groups = { "level.sanity" })
+	public void testCondyDouble() {
+		Assert.assertEquals(PrimitiveCondyMethods.condy_return_double(), 1111111.12D);
+		Assert.assertEquals(PrimitiveCondyMethods.condy_return_double(), 1111111.12D);
+		Assert.assertEquals(PrimitiveCondyMethods.condy_return_double(), 1111111.12D);
+	}
+
+	@Test(groups = { "level.sanity" })
+	public void testCondyString() {
+		Assert.assertEquals(PrimitiveCondyMethods.condy_return_string(), "world");
+		Assert.assertEquals(PrimitiveCondyMethods.condy_return_string(), "world");
+		Assert.assertEquals(PrimitiveCondyMethods.condy_return_string(), "world");
+	}
+}
