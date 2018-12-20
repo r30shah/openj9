16fbf9498122b532bfce4d02c2c0667ac5e9a726
Rahil Shah <rahil@ca.ibm.com>
2018-12-20 18:02:49 -0500

    Set cpIndex to static Class sym ref
    If cpIndex is passed while creating sym ref for static Class
    use it.
    
    Signed-off-by: Rahil Shah <rahil@ca.ibm.com>

--- runtime/compiler/compile/J9SymbolReferenceTable.cpp
+++ runtime/compiler/compile/J9SymbolReferenceTable.cpp
@@ -1171,7 +1171,7 @@ J9::SymbolReferenceTable::findOrCreateClassStaticsSymbol(TR::ResolvedMethodSymbo
    if (!TR::Compiler->cls.classObjectsMayBeCollected())
       sym->setNotCollected();
 
-   symRef = new (trHeapMemory()) TR::SymbolReference(self(), sym, owningMethodSymbol->getResolvedMethodIndex(), -1);
+   symRef = new (trHeapMemory()) TR::SymbolReference(self(), sym, owningMethodSymbol->getResolvedMethodIndex(), cpIndex);
 
    aliasBuilder.addressStaticSymRefs().set(symRef->getReferenceNumber()); // add the symRef to the statics list to get correct aliasing info
 
