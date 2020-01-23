5d276fb0baaca1f9f44ed04199fb2768a89bc013
Rahil Shah <rahil@ca.ibm.com>
2020-01-23 10:51:16 -0500

    Add some JProf fix
    Signed-off-by: Rahil Shah <rahil@ca.ibm.com>

--- runtime/compiler/runtime/J9ValueProfiler.hpp
+++ runtime/compiler/runtime/J9ValueProfiler.hpp
@@ -692,7 +692,6 @@ TR_EmbeddedHashTable<T, bits>::reset()
 template <typename T, size_t bits> void
 TR_EmbeddedHashTable<T, bits>::addKey(T value)
    {
-   size_t otherIndex = this->getOtherIndex();
 
    static bool dumpInfo = feGetEnv("TR_JProfilingValueDumpInfo") != NULL;
    if (dumpInfo)
@@ -706,12 +705,13 @@ TR_EmbeddedHashTable<T, bits>::addKey(T value)
    // Lock the table, disabling jitted code access
    if (!this->tryLock(true))
       {
-      _freqs[otherIndex]++;
+      _freqs[this->getOtherIndex()]++;
       return;
       }
 
    TR_ASSERT_FATAL(this->_metaData.lock == 1, "HashTable not successfully locked");
    TR_ASSERT_FATAL(this->_metaData.otherIndex > -1, "JIT access not disabled");
+   size_t otherIndex = this->getOtherIndex();
 
    // Detect whether the value is already present, count populated slots and find the first empty one
    size_t lastIndex = (1 << bits) - 1;
@@ -719,7 +719,7 @@ TR_EmbeddedHashTable<T, bits>::addKey(T value)
    size_t populated = 0;
    size_t i = 0;
    bool foundVal = false;
-   for (; i < 1 << bits; ++i)
+   for (; i <= lastIndex; ++i)
       {
       if (i == otherIndex)
          continue;
@@ -738,9 +738,6 @@ TR_EmbeddedHashTable<T, bits>::addKey(T value)
       ++populated;
       }
 
-   // If found, increment the value
-//   if (i <= lastIndex)
-//      _freqs[i]++;
    if (!foundVal && available > -1 && populated < this->getCapacity())
       {
       // Temporary hash config
