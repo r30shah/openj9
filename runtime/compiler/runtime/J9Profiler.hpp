db7d5c5de0c30fcc5fbe7fa8a5b6daaaa1c00e28
Rahil Shah <rahil@ca.ibm.com>
2026-07-31 11:59:40 -0400

    Aff TR_JProfValueSites object in TR_ValueProfileInfo

--- runtime/compiler/runtime/J9Profiler.hpp
+++ runtime/compiler/runtime/J9Profiler.hpp
@@ -521,9 +521,13 @@ public:
 
     void dumpInfo(OMR::Logger *log);
 
+    void addJProfValueSites(TR_JProfValueSites *patchSites) { _jProfValueProfSites = patchSites; }
+    TR_JProfValueSites *getJProfValueSites() { return _jProfValueProfSites; }
+
 private:
     TR_AbstractProfilerInfo *_values[LastProfiler];
     TR_CallSiteInfo *_callSiteInfo;
+    TR_JProfValueSites *_jProfValueProfSites;
 };
 
 TR_ValueProfileInfo *TR_ValueProfileInfo::get(TR_PersistentProfileInfo *profileInfo)
