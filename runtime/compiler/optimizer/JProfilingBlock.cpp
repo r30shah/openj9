5c8ac2039d398139e8eabaf3b5ea344303357c09
Rahil Shah <rahil@ca.ibm.com>
2026-09-09 17:08:58 -0400

    Perform JProfiling Block for patchable JProfiling

--- runtime/compiler/optimizer/JProfilingBlock.cpp
+++ runtime/compiler/optimizer/JProfilingBlock.cpp
@@ -910,10 +910,9 @@ int32_t TR_JProfilingBlock::perform()
         logprints(trace(), log, "JProfiling has been enabled, run JProfiling\n");
     } else if (comp()->getProfilingMode() == JProfiling) {
         logprints(trace(), log, "JProfiling has been enabled for profiling compilations, run JProfilingBlock\n");
-    } else if (comp()->getOptimizationPlan()->insertPatchableJProfiling() {
+    } else if (comp()->getOptimizationPlan()->insertPatchableJProfiling()) {
         logprints(trace(), log, "Patchable JProfiling has been enabled - run JProfiling\n");
-    }
-    else {
+    } else {
         logprints(trace(), log, "JProfiling has not been enabled, skip JProfilingBlock\n");
         comp()->setSkippedJProfilingBlock();
         return 0;
